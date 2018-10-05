58f99512893b3a75ccb4734f55e189971f965bb9
Evelyn Gonzalez <evieglz369@gmail.com>
2018-10-05 18:45:32 -0500

    added files

---
+++ app/data/friends.js
@@ -0,0 +1,124 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the possible friends.
+// ===============================================================================
+
+
+var friends = [
+    {
+      name: "Dean Winchester",
+      photo: "http://www.thecelebritycastle.com/wp-content/uploads/2018/04/Dean-Winchester-3.jpg",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Sam Winchester",
+      photo: "https://vignette.wikia.nocookie.net/p__/images/b/b2/Sammy_Winchester.jpg/revision/latest?cb=20180710133834&path-prefix=protagonist",
+      scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+      ]
+    },
+    {
+      name: "Bobby Singer",
+      photo: "https://vignette.wikia.nocookie.net/supernatural/images/f/f4/709-Bobby-Singer.jpg/revision/latest?cb=20111105041652",
+      scores: [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+      ]
+    },
+    {
+      name: "Charlie Bradbury",
+      photo: "http://thegeekmates.files.wordpress.com/2012/05/felicia-day.jpg",
+      scores: [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+    },
+    {
+      name: "Crowley",
+      photo: "https://www.hjackets.com/wp-content/uploads/2017/05/crowley-coat.jpg",
+      scores: [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+      ]
+    },
+    {
+      name: "Castiel",
+      photo: "https://vignette.wikia.nocookie.net/supernatural/images/7/7e/Castiel_807.jpg/revision/latest?cb=20121027012227",
+      scores: [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+      ]
+    },
+    {
+        name: "Garth Fitzgerald",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKPrSsgG5ys1_I6fEtgIV20l4Vkor5hDVv8cEHBvD6_3hUUXCE",
+        scores: [
+          "5",
+          "5",
+          "3",
+          "4",
+          "5",
+          "2",
+          "3",
+          "2",
+          "4",
+          "5"
+        ]
+      }
+  
+  ];
+  
+  // Here we export the array. This makes it accessible to other files using require.
+  module.exports = friends;
